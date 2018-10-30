69e420d876258c6455c8b0d67a3fa9ee3741e25b
amazoradi <amazoradi@gmail.com>
2018-10-30 09:03:21 -0500

    fixed a typo that was stopping the code from working properly

--- audrey.js
+++ audrey.js
@@ -1,10 +1,8 @@
-const audrey = document.getElementById("audrey")
+let audrey = document.getElementById("audrey")
 
 document.addEventListener("scroll", () => {
-  
-    audrey.style.minWidth = "50px"
-    audrey.style.width = window.screenY/3 + "px"
-    audrey.style.minHeight = "100px"
-    audrey.style.height = window.screenY/4 +"px"
- 
+  audrey.style.minWidth = "50px"
+  audrey.style.minHeight = "100px"
+  audrey.style.width = window.scrollY / 3 + "px"
+  audrey.style.height = window.scrollY / 4 + "px"
 })
